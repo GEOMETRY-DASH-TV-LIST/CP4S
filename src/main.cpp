c5e309576b217db235c95dd4cc003ff6bd6c2ddb
GEOMETRY DASH TV LIST <pepet7077@gmail.com>
2026-06-03 17:01:12 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,63 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+// Variables globales para el estado del CPS
+int g_clicksInInterval = 0;
+CCLabelBMFont* g_cpsLabel = nullptr;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+class $modify(MyPlayLayer, PlayLayer) {
+    
+    bool init(GJGameLevel* level, bool useReplay, bool dontRunLevel) {
+        if (!PlayLayer::init(level, useReplay, dontRunLevel)) return false;
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+        // Resetear el contador de clicks al iniciar o reiniciar el nivel
+        g_clicksInInterval = 0;
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        // 1. Crear el texto usando la fuente estilizada de los menús (goldFont)
+        g_cpsLabel = CCLabelBMFont::create("CPS: 0.0", "goldFont.fnt");
+        
+        // Obtener las dimensiones exactas de la pantalla del dispositivo
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        
+        // Posicionar el elemento arriba en la esquina derecha de la pantalla (con margen)
+        g_cpsLabel->setPosition({ winSize.width - 55.0f, winSize.height - 15.0f });
+        
+        // Escalarlo adecuadamente para pantallas táctiles de celulares
+        g_cpsLabel->setScale(0.55f);
+        g_cpsLabel->setOpacity(190); // Sutilmente transparente
+        
+        // Añadirlo a la capa con máxima prioridad visual (z-order: 999)
+        this->addChild(g_cpsLabel, 999);
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        // 2. Programar la rutina que se ejecuta estrictamente cada 4 segundos
+        this->schedule(schedule_selector(MyPlayLayer::updateCpsInterval), 4.0f);
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        return true;
+    }
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+    // Registrar la pulsación táctil (Player 1)
+    void pushButton(PlayerButton button, bool isPlayer2) {
+        PlayLayer::pushButton(button, isPlayer2);
+        
+        if (!isPlayer2 && button == PlayerButton::Jump) {
+            g_clicksInInterval++;
+        }
+    }
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+    // Función ejecutada por el planificador de Cocos2d-x cada 4 segundos
+    void updateCpsInterval(float dt) {
+        if (g_cpsLabel) {
+            // Clicks del lapso divididos entre los 4 segundos transcurridos
+            float cps = static_cast<float>(g_clicksInInterval) / 4.0f;
+            
+            // Re-escribir el texto manteniendo 1 decimal de precisión
+            std::string cpsText = fmt::format("CPS: {:.1f}", cps);
+            g_cpsLabel->setString(cpsText.c_str());
+        }
+        
+        // REINICIO OBLIGATORIO: Volver a comenzar la cuenta para el siguiente ciclo de 4 segundos
+        g_clicksInInterval = 0;
+    }
 };
